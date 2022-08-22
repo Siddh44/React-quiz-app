2bcda15b3aa2405de753f36c795ebe8624e008d3
Siddh44 <vsaisidhartha@gmail.com>
2022-08-22 15:50:31 +0530

    Updates

---
+++ precache-manifest.7caa52342329aee154d14088d3ffb41c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b67cebd496e466d4ce29c463f7ac0c49",
+    "url": "/React-quiz-app/index.html"
+  },
+  {
+    "revision": "a5677f605b4a4962e39b",
+    "url": "/React-quiz-app/static/css/main.ec4ec61e.chunk.css"
+  },
+  {
+    "revision": "7f623081b87268b14ef9",
+    "url": "/React-quiz-app/static/js/2.dfcdf592.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-quiz-app/static/js/2.dfcdf592.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5677f605b4a4962e39b",
+    "url": "/React-quiz-app/static/js/main.3f054945.chunk.js"
+  },
+  {
+    "revision": "53c95ba128e452459fd1",
+    "url": "/React-quiz-app/static/js/runtime-main.d3604482.js"
+  }
+]);
